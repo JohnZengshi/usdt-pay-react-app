902ac26158979000d845a9e53eccb0e5b5b383ad
john <johnzengshi@gmail.com>
2025-01-02 14:39:44 +0800

    feat: 添加 Emotion 库实例及 px 转 rem 功能

---
+++ src/lib/emotion.ts
@@ -0,0 +1,42 @@
+import createEmotion from "@emotion/css/create-instance";
+// Convert px to rem
+function px2rem(value: string) {
+  value = value.replace("px", "");
+  return (parseFloat(value) / 37.5).toFixed(2) + "rem"; // 1rem = 37.5px
+}
+// Create an instance of emotion
+export const {
+  flush,
+  hydrate,
+  cx,
+  merge,
+  getRegisteredStyles,
+  injectGlobal,
+  keyframes,
+  css,
+  sheet,
+  cache,
+} = createEmotion({
+  key: "emotion", // key for emotion cache
+  stylisPlugins: [
+    // stylis plugins
+    (e) => {
+      if (typeof e.value == "string")
+        e.value = e.value.replace(/(\d+(\.\d+)?)px/g, (_, p1) => px2rem(p1)); // replace px with rem
+      // console.log(e);
+    },
+  ],
+});
+
+export default {
+  flush,
+  hydrate,
+  cx,
+  merge,
+  getRegisteredStyles,
+  injectGlobal,
+  keyframes,
+  css,
+  sheet,
+  cache,
+};
