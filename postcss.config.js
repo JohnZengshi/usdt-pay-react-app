21a4d3cebc977bd94f102286cb76d941f44af0a3
john <johnzengshi@gmail.com>
2025-01-02 14:40:29 +0800

    feat: 添加 postcss-pxtorem 插件以支持 px 转 rem，更新依赖项

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,13 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-pxtorem": {
+      rootValue: 37.5,
+      propList: ["*"],
+      exclude: (path) => {
+        // console.log(path);
+        return false;
+      },
+    },
   },
-}
+};
